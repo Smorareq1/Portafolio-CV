fad272a3f116d41212cc1e6c6d42058c55b777cc
Smorareq1 <smorareq1@gmail.com>
2024-04-19 11:00:10 -0600

    Terminado el footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,8 +69,6 @@ tabs.forEach(tab => {
     })
 })
 
-/*==================== SERVICES MODAL ====================*/
-
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
@@ -79,7 +77,24 @@ tabs.forEach(tab => {
 
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+
+}
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
 
